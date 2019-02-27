45f82c76318cd36af00eb9d32975abb32d52c4cc
superbeom <it.super.beom@gmail.com>
2019-02-27 14:06:18 +0900

    middle commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+
+class App extends Component {
+  render() {
+    return <Navbar />;
+  }
+}
+
+export default App;
---
+++ src/TourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+  },
+  {
+    id: 2,
+    city: "seoul",
+    img: "./img/seoul.jpeg",
+    name: "seoul palace tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+  },
+  {
+    id: 3,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+  },
+  {
+    id: 4,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london bridge tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+  }
+];
